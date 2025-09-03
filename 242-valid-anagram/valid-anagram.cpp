3979c6d70789763cb283161b9b6f8128e198a54c
Khushi Yadav <ky0307005@gmail.com>
2025-09-03 11:34:24 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (48.43%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,11 +1,24 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if (s.size() != t.size()) return false;
+        if(s.length()!=t.length()){
+            return false;
+        }
 
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
+        int count[26]={0};
+        for(int i=0;i<s.length();i++){
+            int idx=s[i]-'a';
+            count[idx]++;
+        }
+        for(int i=0;i<t.length();i++){
+            int idx=t[i]-'a';
+            if(count[idx]==0){
+                return false;
+            }
+            count[idx]--;
+        }
+        return true;
 
-        return s == t;
+        
     }
 };
